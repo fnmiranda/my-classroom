998acfbf26583ccb69c2c1a0a840be4cae14a0ef
fnmiranda <r20franciscomiranda@gmail.com>
2025-11-01 17:50:02 -0300

    create default components as input and button, and configure the initial archives of main layout and the configuration of the initial route login

---
+++ components/Login/Login.js
@@ -0,0 +1,28 @@
+'use-client'
+
+import React from 'react';
+
+const Login = () => {
+
+  return (
+    <div>
+      <div>
+        Welcome to the most reliable Notes BR
+      </div>
+      <div>
+        Name:
+        <input />
+
+      </div>
+      <div>
+        Password:
+        <input />
+      </div>
+      <button>
+        Sign-in
+      </button>
+    </div>
+  )
+}
+
+export default Login;
